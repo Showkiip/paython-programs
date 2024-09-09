f49fa053f8f1f27efc017b97842af68760919210
Showkat Ali <71689825+Showkiip@users.noreply.github.com>
2024-09-09 16:18:48 +0500

    make swipping logic

---
+++ swippingnumbers.py
@@ -0,0 +1,8 @@
+ # swipping two numbers without use temp variables
+a = 5
+b = 8
+
+a = a + b   #add a and b  = 13  assign to a now a is 13
+b = a-b     #  subs the a and b  13 - 8   and b now 5
+a  = a - b  #  sub a and b   13 - 5  and now a 8
+print("a = " ,a , "b = ", b)
